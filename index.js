a4bb7fda81a36e873d7865e366de06e68ad9e1be
MaxZhurakov <muxmax969@gmail.com>
2025-03-02 13:38:58 +0100

    24.1.2025

--- index.js
+++ index.js
@@ -1,31 +1,94 @@
-//Цикл у супермаркеті
-const products = ["хліб", "молоко", "яблука", "шоколад", "банани"];
-let i = 0;
+//1
+let age = parseInt(prompt("Введіть ваш вік:"));
 
-while (i < products.length) {
-    const product = products[i];
-    console.log(`Додаємо ${product} у кошик...`);
-
-    if (product === "шоколад") {
-        console.log("Солодке життя забезпечене!");
-    }
-
-    i++;
+if (age >= 0 && age <= 12) {
+    console.log("Ви дитина");
+} else if (age > 12 && age <= 18) {
+    console.log("Ви підліток");
+} else if (age > 18 && age <= 60) {
+    console.log("Ви дорослий");
+} else if (age > 60) {
+    console.log("Ви пенсіонер");
+} else {
+    console.log("Невірний вік");
 }
 
-console.log("Всі продукти у кошику! Тепер можна йти на касу.");
+//2
+let digit = parseInt(prompt("Введіть число від 0 до 9:"));
+let symbol;
 
-function multiplyNumbers(num) {
-    if (num < 1 || num > 100) {
-        console.log("Це фабрика для реальних чисел! Давай щось нормальне 😏");
-        return;
-    }
-//Математична фабрика
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${num} * ${i} = ${num * i}`);
-    }
+switch (digit) {
+    case 0:
+        symbol = ")";
+        break;
+    case 1:
+        symbol = "!";
+        break;
+    case 2:
+        symbol = "@";
+        break;
+    case 3:
+        symbol = "#";
+        break;
+    case 4:
+        symbol = "$";
+        break;
+    case 5:
+        symbol = "%";
+        break;
+    case 6:
+        symbol = "^";
+        break;
+    case 7:
+        symbol = "&";
+        break;
+    case 8:
+        symbol = "*";
+        break;
+    case 9:
+        symbol = "(";
+        break;
+    default:
+        symbol = "Невірне число";
 }
 
-multiplyNumbers(5);
-multiplyNumbers(101);
-multiplyNumbers(-3);
+console.log(symbol);
+
+//3
+let num = prompt("Введіть тризначне число:");
+
+if (num.length === 3) {
+    let digit1 = num[0];
+    let digit2 = num[1];
+    let digit3 = num[2];
+
+    if (digit1 === digit2 || digit1 === digit3 || digit2 === digit3) {
+        console.log("Число містить однакові цифри");
+    } else {
+        console.log("Число не містить однакових цифр");
+    }
+} else {
+    console.log("Невірне число");
+}
+
+//4
+let year = parseInt(prompt("Введіть рік:"));
+
+if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
+    console.log("Рік високосний");
+} else {
+    console.log("Рік не високосний");
+}
+
+//5
+let palindromeNumber = prompt("Введіть п'ятирозрядне число:");
+
+if (palindromeNumber.length === 5) {
+    if (palindromeNumber[0] === palindromeNumber[4] && palindromeNumber[1] === palindromeNumber[3]) {
+        console.log("Число є паліндромом");
+    } else {
+        console.log("Число не є паліндромом");
+    }
+} else {
+    console.log("Невірне число");
+}
